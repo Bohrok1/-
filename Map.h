cb443dcef0c8fbf2f15765362e8c5e5e52c4f528
Bohrok1 <65021992+Bohrok1@users.noreply.github.com>
2020-05-28 18:18:46 +0300

    Add files via upload

---
+++ Map.h
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+const int height = 18;
+const int width = 26;
+
+char TileMap[height][width] = {
+"           al  a         ",
+"           l     dd      ",
+"                 tt      ",
+"ooooooooooooooootqq      ",
+"ooooooooooooooooooq      ",
+"ooooooooooooooooooq      ",
+"tooooooooooooootooq      ",
+"qooooppoyyooootq1oq      ",
+"qtttttttttttttqqttq     q",
+"                        q",
+"                        q",
+"                        q",
+"    s                   q",
+"    s     2			 q",
+"ttttttttttttttttttttttttt"
+};
